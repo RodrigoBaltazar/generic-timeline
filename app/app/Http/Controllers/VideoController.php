9c1af93e2445479717042c756bbc6187b41e4ca9
RodrigoBaltazar <rodrigobaltazarinf@gmail.com>
2023-06-12 21:35:11 -0300

    fim do bootcamp

---
+++ app/app/Http/Controllers/VideoController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Video;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class VideoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response 
+    {
+        return Inertia::render('Videos/Index', [
+
+            'videos' => Video::with('user:id,name')->latest()->get(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+
+            'message' => 'required|string|max:255',
+
+        ]);
+
+ 
+
+        $request->user()->videos()->create($validated);
+
+ 
+
+        return redirect(route('videos.index'));    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Video $video)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Video $video)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Video $video): RedirectResponse    {
+        $this->authorize('update', $video);
+
+ 
+
+        $validated = $request->validate([
+
+            'message' => 'required|string|max:255',
+
+        ]);
+
+ 
+
+        $video->update($validated);
+
+ 
+
+        return redirect(route('videos.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Video $video): RedirectResponse    
+    {
+        $this->authorize('delete', $video);
+
+ 
+
+        $video->delete();
+
+ 
+
+        return redirect(route('videos.index'));
+    }
+}
